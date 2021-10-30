d60e09f8525d7110931f34e72882cfa83d103b27
Evgeny Nerush <eugene.nerush@gmail.com>
2021-10-30 21:15:18 +0300

    like initial commit

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,4 @@
-struct Point {
-    x: f64,
-    y: f64,
-}
+use quelea::{Point};
 
 fn main() {
     let p = Point { x: 3.0, y: 4.0 };
---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub struct Point {
+    pub x: f64,
+    pub y: f64,
+}
