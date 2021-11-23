921402939760502599ffe37b0e13260ab63e01b3
SerebryakovMA <serebryakov17m@mail.ru>
2021-11-23 23:48:39 +0300

    Zeldovich's solution was added

--- rust-old/python/examples/zeldovich.py
+++ rust-old/python/examples/zeldovich.py
@@ -3,13 +3,15 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+import scipy.constants as cns
+
 
 import sys
 sys.path.append("../")
 from quelea import *
 
-a0 = 137
-wavelength = 0.1
+a0 = 1500
+wavelength = 1
 
 #initial momenta and coords of the electron
 s0 = [0, 0, 0, 0, 0, 0]
@@ -18,18 +20,27 @@ t_end = 100 # lambda / c
 dt = 0.01
 nt = int( np.round(t_end / dt) )
 
-traj_n = 10000
+traj_n = 1000
 
 def phase_portrate(traj_n, a0, wavelength, s0, nt, dt):
     abscissa = np.empty(traj_n)
     ordinate = np.empty(traj_n)
-
+    A = 2/3 *cns.alpha*cns.hbar/np.power(cns.c,2)*a0**3
+    p = (np.sqrt(np.sqrt(4*np.power(A,2)+1)-1))/(2*np.power(A,2))
+    
     for i in range(traj_n):
         traj = drive_particle(s0, 0, dt, nt, 3, [a0, 0, 0, 1, a0, 0, 0], wavelength, 1) 
         abscissa[i] = ux(traj)[-1]
         ordinate[i] = uy(traj)[-1]
 
-    plt.hist2d(abscissa, ordinate, bins=[100,100], cmap=plt.cm.jet, label=r"a_0 = 800, \lambda = 0.01")
+    traj = drive_particle(s0, 0, dt, nt, 3, [a0, 0, 0, 1, a0, 0, 0], wavelength, 1)
+    phi = t(0, dt, traj)
+
+    u_x = -np.power(p,3)*A*np.cos(phi) - np.power(p,2)*np.sin(phi)
+    u_y = -np.power(p,3)*A*np.sin(phi) + np.power(p,2)*np.cos(phi)
+
+    #plt.hist2d(abscissa, ordinate, bins=[100,100], cmap=plt.cm.jet, label=r"a_0 = 800, \lambda = 0.01")
+    plt.plot(u_x, u_y)
     plt.xlabel(r"$p_x$")
     plt.ylabel(r"$p_y$")
     #plt.plot(t(0, dt, traj), g(traj))
@@ -45,6 +56,8 @@ def spiral_plot(a0, wavelength, s0, nt, dt):
     plt.xlabel(r'$p_\parallel$')
     plt.ylabel(r'$p_\perp$')
     plt.savefig("pp_ort_" + str(a0) + "_" + str(wavelength) + ".png")
+
+
 
 def main():
     #for a0 in []:
